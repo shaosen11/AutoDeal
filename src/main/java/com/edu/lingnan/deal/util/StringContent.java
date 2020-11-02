c8fd3f68ed07983eed335352394da85e5f9afc7a
Shao <1244785737@qq.com>
2020-11-02 09:08:42 +0800

    修改拼接字段

--- src/main/java/com/edu/lingnan/deal/util/StringContent.java
+++ src/main/java/com/edu/lingnan/deal/util/StringContent.java
@@ -3,7 +3,7 @@ package com.edu.lingnan.deal.util;
 public class StringContent {
     public static String lianlubucunzai = "--链路不存在";
     public static String sulvbucunzai = "--速率不存在";
-    public static String lianlubuqueding = "--没有“/”，确定存在速率？";
+    public static String lianlubuqueding = "====没有“/”，确定是链路？====";
     public static String qingchaxun = "=====请查询=====";
 
 }
