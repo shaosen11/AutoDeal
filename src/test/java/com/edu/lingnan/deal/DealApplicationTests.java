76cd55d19c2494d7985079d6c167709529e8ae52
Shao_sen <1244785737@qq.com>
2020-12-08 18:03:59 +0800

    解决下标越界问题

--- src/test/java/com/edu/lingnan/deal/DealApplicationTests.java
+++ src/test/java/com/edu/lingnan/deal/DealApplicationTests.java
@@ -16,7 +16,34 @@ public class DealApplicationTests {
 
     @Test
     public void contextLoads() {
-        System.out.println(lianluService.getByName("HKG/CT-SHI/CT VC4S044/2M22"));
+        String str1 = "广东省, M宽带,\"\", 1个月";
+        addDoubleQuotationMarks(str1);
+        String str2 = "广东省";
+        addDoubleQuotationMarks(str2);
+        String str3 = "\"\"广东省";
+        addDoubleQuotationMarks(str3);
+    }
+
+    private String addDoubleQuotationMarks(String str) {
+        System.out.println(str);
+        StringBuffer newStr = new StringBuffer(str);
+        //处理"
+        if (newStr.indexOf("\"") != -1) {
+            for (int i = 0; i < newStr.length(); i++) {
+                if (newStr.charAt(i) == '"') {
+                    newStr.insert(i, '"');
+                    i++;
+                }
+            }
+        }
+        //处理,
+        if (newStr.indexOf(",") != -1) {
+            newStr.insert(0, '"');
+            newStr.insert(newStr.length(), '"');
+        }
+        str = newStr.toString();
+        System.out.println(str);
+        return str;
     }
 
 }
